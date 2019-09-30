6bf2dee868e1dc2fada1cb07b2c873d57e7f8f04
Mikhail Burilov <burilovmv@gmail.com>
2019-09-30 07:28:04 +0300

    Added data loader, test harness config, changed CI script

---
+++ AzureSqlSupplyCollectorLoader/AzureSqlSupplyCollectorLoader.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Text;
+using S2.BlackSwan.SupplyCollector.Models;
+using SupplyCollectorDataLoader;
+
+namespace AzureSqlSupplyCollectorLoader
+{
+    public class AzureSqlSupplyCollectorLoader : SupplyCollectorDataLoaderBase {
+        public override void InitializeDatabase(DataContainer dataContainer) {
+            using (var conn = new SqlConnection(dataContainer.ConnectionString)) {
+                conn.Open();
+
+                //create database TestDb;
+                //go
+
+            }
+        }
+        public override void LoadSamples(DataEntity[] dataEntities, long count) {
+            using (var conn = new SqlConnection(dataEntities[0].Container.ConnectionString)) {
+                conn.Open();
+
+                var sb = new StringBuilder();
+                sb.Append("CREATE TABLE ");
+                sb.Append(dataEntities[0].Collection.Name);
+                sb.Append(" (\n");
+                sb.Append($"id_field IDENTITY(1, 1) CONSTRAINT {dataEntities[0].Collection.Name}_pk PRIMARY KEY");
+
+                foreach (var dataEntity in dataEntities) {
+                    sb.Append(",\n");
+                    sb.Append(dataEntity.Name);
+                    sb.Append(" ");
+
+                    switch (dataEntity.DataType) {
+                        case DataType.String:
+                            sb.Append("text");
+                            break;
+                        case DataType.Int:
+                            sb.Append("int");
+                            break;
+                        case DataType.Double:
+                            sb.Append("float");
+                            break;
+                        case DataType.Boolean:
+                            sb.Append("bit");
+                            break;
+                        case DataType.DateTime:
+                            sb.Append("datetime");
+                            break;
+                        default:
+                            sb.Append("int");
+                            break;
+                    }
+
+                    sb.AppendLine();
+                }
+
+                sb.Append(");");
+
+                using (var cmd = conn.CreateCommand()) {
+                    cmd.CommandText = sb.ToString();
+                    cmd.ExecuteNonQuery();
+                }
+
+                var r = new Random();
+                long rows = 0;
+                while (rows < count) {
+                    long bulkSize = 10000;
+                    if (bulkSize + rows > count)
+                        bulkSize = count - rows;
+
+                    sb = new StringBuilder();
+                    sb.Append("INSERT INTO ");
+                    sb.Append(dataEntities[0].Collection.Name);
+                    sb.Append("(");
+
+                    bool first = true;
+                    foreach (var dataEntity in dataEntities) {
+                        if (!first) {
+                            sb.Append(", ");
+                        }
+
+                        sb.Append(dataEntity.Name);
+                        first = false;
+                    }
+
+                    sb.Append(") VALUES ");
+
+                    for (int i = 0; i < bulkSize; i++) {
+                        if (i > 0)
+                            sb.Append(", ");
+
+                        sb.Append("(");
+                        first = true;
+                        foreach (var dataEntity in dataEntities) {
+                            if (!first) {
+                                sb.Append(", ");
+                            }
+
+                            switch (dataEntity.DataType) {
+                                case DataType.String:
+                                    sb.Append("'");
+                                    sb.Append(new Guid().ToString());
+                                    sb.Append("'");
+                                    break;
+                                case DataType.Int:
+                                    sb.Append(r.Next().ToString());
+                                    break;
+                                case DataType.Double:
+                                    sb.Append(r.NextDouble().ToString().Replace(",", "."));
+                                    break;
+                                case DataType.Boolean:
+                                    sb.Append(r.Next(100) > 50 ? "true" : "false");
+                                    break;
+                                case DataType.DateTime:
+                                    var val = DateTimeOffset
+                                        .FromUnixTimeMilliseconds(
+                                            DateTimeOffset.Now.ToUnixTimeMilliseconds() + r.Next()).DateTime;
+                                    sb.Append("'");
+                                    sb.Append(val.ToString("s"));
+                                    sb.Append("'");
+                                    break;
+                                default:
+                                    sb.Append(r.Next().ToString());
+                                    break;
+                            }
+
+                            first = false;
+                        }
+
+                        sb.Append(")");
+                    }
+
+                    sb.Append(";");
+
+                    using (var cmd = conn.CreateCommand()) {
+                        cmd.CommandText = sb.ToString();
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    rows += bulkSize;
+                    Console.Write(".");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        public override void LoadUnitTestData(DataContainer dataContainer) {
+            using (var conn = new SqlConnection(dataContainer.ConnectionString)) {
+                conn.Open();
+
+                using (var reader = new StreamReader("tests/data.sql")) {
+                    var sb = new StringBuilder();
+                    while (!reader.EndOfStream) {
+                        var line = reader.ReadLine();
+                        if (String.IsNullOrEmpty(line))
+                            continue;
+
+                        if (line.Equals("go"))
+                            continue;
+
+                        sb.AppendLine(line);
+                        if (line.TrimEnd().EndsWith(";")) {
+                            using (var cmd = conn.CreateCommand()) {
+                                cmd.CommandTimeout = 600;
+                                cmd.CommandText = sb.ToString();
+
+                                cmd.ExecuteNonQuery();
+                            }
+
+                            sb.Clear();
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
--- run-tests.sh
+++ run-tests.sh
@@ -2,7 +2,7 @@
 
 docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=<YourStrong@Passw0rd>" -p 1433:1433 --name sql1 -d mcr.microsoft.com/mssql/server:2017-latest
 sleep 20
-docker cp AzureSqlSupplyCollectorTests/tests/data.sql sql1:/data.sql
+docker cp AzureSqlSupplyCollectorLoader/tests/data.sql sql1:/data.sql
 docker exec sql1 /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "<YourStrong@Passw0rd>" -i /data.sql
 
 export AZURE_SQL_HOST=localhost
